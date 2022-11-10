448567f02339a91a0aacb7cec3eea85d77943c6c
anshuldavid <anshuldavid@google.com>
2022-11-10 14:25:08 +0530

    Final changes

--- apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.RetryConformanceTests/RetryConformanceTestFixture.cs
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.RetryConformanceTests/RetryConformanceTestFixture.cs
@@ -35,7 +35,7 @@ public class RetryConformanceTestFixture : CloudProjectFixtureBase, ICollectionF
 
     //internal static string TestBenchUrl { get; } = GetEnvironmentVariableOrDefault("TEST_BENCH_URL", "http://localhost:9000/");
 
-    // TODO: Consider using the normal TEST_PROJECT environment variable (the default) instead.
+    // PROJECT_ID to be set to "test" for storage test bench
     public RetryConformanceTestFixture() : base("PROJECT_ID")
     {
         var clientBuilder = new StorageClientBuilder
