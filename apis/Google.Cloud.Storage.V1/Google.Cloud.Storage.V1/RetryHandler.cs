9ed83d5e2730c071708e2088c3421ba3b9827838
anshuldavid <anshuldavid@google.com>
2022-12-08 23:55:01 +0530

    Logging

--- apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1/RetryHandler.cs
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1/RetryHandler.cs
@@ -64,6 +64,7 @@ namespace Google.Cloud.Storage.V1
 
         public async Task<bool> HandleResponseAsync(HandleUnsuccessfulResponseArgs args)
         {
+            Console.WriteLine(args.Response);
             var retry = args.SupportsRetry && await IsRetriableResponse(args.Response).ConfigureAwait(false);
             if (!retry)
             {
